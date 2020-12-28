a97fbacb590e13fe2fdcd73d1a11425501498aa8
Walter Berggren <walter.berggren@gmail.com>
2020-12-28 23:23:40 +0100

    Add caching to backend

--- backend/fetchFlightData.js
+++ backend/fetchFlightData.js
@@ -1,19 +1,46 @@
 import assert from "assert"
 import https from "https"
 import process from "process"
+import { Semaphore } from "await-semaphore"
 
 const API_KEY = process.env.API_KEY
 assert(API_KEY, "Missing Swedavia API key as API_KEY environment variable")
 
+let flightDataCache = null
+let flightDataLastUpdated = null
+const CACHE_REFRESH_INTERVAL = 60 * 1000
+const cacheUpdateSemaphore = new Semaphore(1)
+
 const fetchFlightData = async (airportIATA) => {
+    if (flightDataCache &&
+        Date.now() - flightDataLastUpdated.getTime() < CACHE_REFRESH_INTERVAL) {
+        console.log("Using cache")
+        return flightDataCache
+    }
+
+    const release = await cacheUpdateSemaphore.acquire()
+
+    if (flightDataCache &&
+        Date.now() - flightDataLastUpdated.getTime() < CACHE_REFRESH_INTERVAL) {
+        // Flight data was already fetched by someone else while we waited for the lock, just return the cached data
+        console.log("Flight data already fetched, using cache")
+        release()
+        return flightDataCache
+    }
+
+    console.log("Fetching fresh data from Swedavia")
     const arrivals = fetch(airportIATA, "arrivals")
     const departures = fetch(airportIATA, "departures")
     const arrivalsAndDepartures = await Promise.all([arrivals, departures])
-
-    return {
+    flightDataCache = {
         arrivals: arrivalsAndDepartures[0],
         departures: arrivalsAndDepartures[1]
     }
+    flightDataLastUpdated = new Date()
+
+    release()
+
+    return flightDataCache
 }
 
 const fetch = (airportIATA, mode) => {
