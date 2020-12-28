3b655b5faedf5ae7c3198f4b2d7ea7770b392144
Walter Berggren <walter.berggren@gmail.com>
2020-12-28 13:36:43 +0100

    Style table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Ankomster</h1>
       </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Tid</th>
+            <th>Från</th>
+            <th>Flygnr</th>
+            <th>Anmärkning</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>14:20</td>
+            <td>Stockholm ARN</td>
+            <td>SK34</td>
+            <td>Landat</td>
+          </tr>
+          <tr>
+            <td>17:25</td>
+            <td>Stockholm ARN</td>
+            <td>SK2030</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>20:30</td>
+            <td>Stockholm BRA</td>
+            <td>TF120</td>
+            <td>Inställd</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
