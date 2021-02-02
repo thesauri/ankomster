970386e28a589caa717f7699b1956af8f1092acb
Walter Berggren <walter@berggren.dev>
2021-02-02 21:45:03 +0200

    Swap h2-link hierarchy

--- frontend/src/AirportPicker.js
+++ frontend/src/AirportPicker.js
@@ -4,36 +4,56 @@ import "./AirportPicker.css"
 const AirportPicker = () => (
   <div className="airport-picker">
     <h1>Flyginfo <span>/ Välj flygplats</span></h1>
-    <Link to="/ARN/arrivals">
-      <h2>Stockholm Arlanda</h2>
-    </Link>
-    <Link to="/GOT/arrivals">
-      <h2>Göteborg Landvetter</h2>
-    </Link>
-    <Link to="/BMA/arrivals">
-      <h2>Stockholm Bromma</h2>
-    </Link>
-    <Link to="/MMX/arrivals">
-      <h2>Malmö</h2>
-    </Link>
-    <Link to="/LLA/arrivals">
-      <h2>Luleå</h2>
-    </Link>
-    <Link to="/UME/arrivals">
-      <h2>Umeå</h2>
-    </Link>
-    <Link to="/OSD/arrivals">
-      <h2>Åre Östersund</h2>
-    </Link>
-    <Link to="/VBY/arrivals">
-      <h2>Visby</h2>
-    </Link>
-    <Link to="/RNB/arrivals">
-      <h2>Ronneby</h2>
-    </Link>
-    <Link to="/KRN/arrivals">
-      <h2>Kiruna</h2>
-    </Link>
+    <h2>
+      <Link to="/ARN/arrivals">
+        Stockholm Arlanda
+      </Link>
+    </h2>
+    <h2>
+      <Link to="/GOT/arrivals">
+        Göteborg Landvetter
+      </Link>
+    </h2>
+    <h2>
+      <Link to="/BMA/arrivals">
+        Stockholm Bromma
+      </Link>
+    </h2>
+    <h2>
+      <Link to="/MMX/arrivals">
+        Malmö
+      </Link>
+    </h2>
+    <h2>
+      <Link to="/LLA/arrivals">
+        Luleå
+      </Link>
+    </h2>
+    <h2>
+      <Link to="/UME/arrivals">
+        Umeå
+      </Link>
+    </h2>
+    <h2>
+      <Link to="/OSD/arrivals">
+        Åre Östersund
+      </Link>
+    </h2>
+    <h2>
+      <Link to="/VBY/arrivals">
+        Visby
+      </Link>
+    </h2>
+    <h2>
+      <Link to="/RNB/arrivals">
+        Ronneby
+      </Link>
+    </h2>
+    <h2>
+      <Link to="/KRN/arrivals">
+        Kiruna
+      </Link>
+    </h2>
   </div>
 )
 
