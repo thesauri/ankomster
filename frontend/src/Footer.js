e266a6f9eca17f2eb622c8c27d9602edb2ebb5da
Walter Berggren <walter@berggren.dev>
2021-02-04 19:15:01 +0200

    Swap footer items

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -2,8 +2,8 @@ import "./Footer.css"
 
 const Footer = () => (
   <footer className="footer">
-    <p>© {(new Date()).getFullYear()} <a href="https://berggren.dev">Walter Berggren</a></p>
     <p>Fork me on <a href="https://github.com/thesauri/flyginfo">GitHub</a></p>
+    <p>© {(new Date()).getFullYear()} <a href="https://berggren.dev">Walter Berggren</a></p>
   </footer>
 )
 
